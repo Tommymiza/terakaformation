bd693a705014cdf9fb9e1375feef7eeb67506de3
Tommymiza <tommymiza20@gmail.com>
2023-02-11 13:21:44 +0300

    Navbar

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import axios from "axios";
 import Formation from "./pages/Formation";
 import Connexion from "./components/Connexion";
 import Info from "./components/Info"
+import { CircularProgress } from "@mui/material";
 
 export const ActContext = createContext();
 
 function App() {
   const [user, setUser] = useState();
   const [alert, setAlert] = useState();
+  const [loading, setLoading] = useState(true);
   const server = "https://api.teraka.org";
   useEffect(()=>{
     if(alert){
@@ -34,7 +36,11 @@ function App() {
         })
         .catch((err) => {
           console.log(err);
-        });
+        }).finally(()=>{
+          setLoading(false);
+        })
+    }else{
+      setLoading(false)
     }
   }, []);
   return (
@@ -43,7 +49,13 @@ function App() {
         <Navbar />
       </header>
       <ActContext.Provider value={{server, user, setUser, setAlert}}>
-        <section>{user ? <Formation /> : <Connexion />}</section>
+        {loading ? (
+          <div className="loading">
+            <CircularProgress size={150} />
+          </div>
+        ) : (
+          <section>{user ? <Formation /> : <Connexion />}</section>
+        )}
       </ActContext.Provider>
       {alert && <Info type={alert.type} message={alert.message} />}
     </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,9 +9,9 @@ export default function Navbar () {
           <img src="logo.png" alt="Teraka logo" style={{width: 100}} />
         </div>
         <ul className='navbar d-flex flex-row col-md-4 col-sm-6 justify-content-around align-items-center gap-3 m-0'>
-          <li title="Page d'accueil"><a href="https://teraka.vercel.app">Accueil</a></li>
-          <li title="Le programme TERAKA"><a href="https://terakaprogram.vercel.app">Programme</a></li>
-          <li title="Page des formations"><a href="https://terakajoin.vercel.app">Rejoindre</a></li>
+          <li title="Page d'accueil"><a href="https://www.teraka.org">Accueil</a></li>
+          <li title="Le programme TERAKA"><a href="https://programme.teraka.org">Programme</a></li>
+          <li title="Page des formations"><a href="https://rejoindre.teraka.org">Rejoindre</a></li>
         </ul>
       </AppBar>
     </Box>
